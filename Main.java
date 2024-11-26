613133a4badb14d2f772937f4b037abfeb71532f
Ashish Singh <iasish777@gmail.com>
2024-11-26 08:47:02 +0530

    Update and rename Practice1.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Practice1 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
